76f561c01ca1709b189fa22fa622ede53094264b
Yusei Mizutani <yusei4520030401@gmail.com>
2023-08-17 10:58:57 +0900

    fix: 線の削除

--- components/ui/submissionResultsList/SubmissionResultsList.tsx
+++ components/ui/submissionResultsList/SubmissionResultsList.tsx
@@ -6,77 +6,77 @@ export default function SubmissionResultsList() {
       <Title mb={40} order={1} align="center" weight="bold">
         投稿実績
       </Title>
-      <SimpleGrid cols={8} spacing="xs" verticalSpacing={60}>
-        <Box h={100} w={100} bg="blue">
+      <SimpleGrid cols={8} spacing={16} verticalSpacing={20}>
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
-        <Box h={100} w={100} bg="blue">
+        <Box h={80} w={80} bg="blue">
           <Text align="center">Java</Text>
         </Box>
       </SimpleGrid>
