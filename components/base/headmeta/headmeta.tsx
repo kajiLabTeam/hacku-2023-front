3d407888b8ba896adffa8f4b59299528a518bae7
SatooRu65536 <ryuto4619@gmail.com>
2023-08-18 01:35:03 +0900

    add: Twitter card を追加

--- components/base/headmeta/headmeta.tsx
+++ components/base/headmeta/headmeta.tsx
@@ -15,6 +15,7 @@ export default function HeadMeta() {
       <meta property="og:type" content="website" />
       <meta property="og:url" content={SITE_URL} />
       <meta property="og:image" content={IMG_URL} />
+      <meta name="twitter:image" content={IMG_URL} />
       <meta property="og:site_name" content={TITLE} />
       <meta name="twitter:image" content="summary" />
       <link rel="icon" href="/favicon.ico" />
