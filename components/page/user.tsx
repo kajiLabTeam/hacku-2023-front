807d510e38f0fbd928a7fcf787496558175e41dc
SatooRu65536 <ryuto4619@gmail.com>
2023-08-18 01:20:01 +0900

    fix: ページ遷移後の再生

--- components/page/user.tsx
+++ components/page/user.tsx
@@ -1,6 +1,14 @@
+import { usePlayingSound } from "@/hooks/usePlayingSound";
 import styles from "@/styles/user.module.scss";
+import { useEffect } from "react";
 
 export default function UserPage() {
+  const [_, { switchShort }] = usePlayingSound();
+
+  useEffect(() => {
+    switchShort(-1);
+  }, [switchShort]);
+  
   return (
     <section className={styles.user}>
       <h1>User</h1>
