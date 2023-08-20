580177ddcecd5878c364d91ed4f5664e11830a9d
Yusei Mizutani <yusei4520030401@gmail.com>
2023-08-21 04:40:54 +0900

    add: 投稿履歴と閲覧履歴をカルーセルで表示するよう実装

--- components/ui/userPage/logReport/reportList/ReportList.tsx
+++ components/ui/userPage/logReport/reportList/ReportList.tsx
@@ -1,34 +1,67 @@
-import { Box, SimpleGrid, Text, Title } from "@mantine/core";
-
 import { useRecoilValue } from "recoil";
-import { postHistoryState } from "@/store/state";
-import LogSlideComponent from "../slide/logSlide";
+import { Carousel } from "@mantine/carousel";
+import { Box, SimpleGrid, Text } from "@mantine/core";
+
+import styles from "./reportList.module.scss";
 import { useUserPage } from "@/hooks/useUserPage";
+import LogSlideComponent from "../slide/logSlide";
+import { browsingHistoryState, postHistoryState } from "@/store/state";
 
 export default function ReportList() {
   const { formatNumber } = useUserPage();
   const postHistory = useRecoilValue(postHistoryState);
+  const browsingHistory = useRecoilValue(browsingHistoryState);
 
   return (
-    <Box ml={80}>
-      <Title mt={40} order={1} align="center" weight="bold">
-        自分の投稿
-      </Title>
-      <SimpleGrid cols={2} mt="xl" verticalSpacing={40}>
-        {postHistory.postHistories.map((postHistory) => {
-          return (
-            <Box key={postHistory.id}>
-              <LogSlideComponent
-                markdown={postHistory.slide}
-                views={formatNumber(postHistory.views)}
-              />
-              <Text mt={10} fw={800} align="center">
-                {postHistory.title}
-              </Text>
-            </Box>
-          );
+    <Box>
+      <Carousel
+        slideGap="xl"
+        sx={(theme) => ({
+          "@media (min-width: 850px)": {
+            marginBottom: 20,
+          },
+          "@media (max-width: 850px)": {
+            marginBottom: 80,
+          },
         })}
-      </SimpleGrid>
+      >
+        <Carousel.Slide>
+          <h1 className={styles.title_in_card}>自分の閲覧履歴</h1>
+          <SimpleGrid mb={40} cols={2} mt="xl" verticalSpacing={40}>
+            {browsingHistory.browsingHistories.map((browsingHistory) => {
+              return (
+                <Box key={browsingHistory.id}>
+                  <LogSlideComponent
+                    markdown={browsingHistory.slide}
+                    views={formatNumber(browsingHistory.views)}
+                  />
+                  <Text mt={10} fw={800} align="center">
+                    {browsingHistory.title}
+                  </Text>
+                </Box>
+              );
+            })}
+          </SimpleGrid>
+        </Carousel.Slide>
+        <Carousel.Slide>
+          <h1 className={styles.title_in_card}>過去の投稿</h1>
+          <SimpleGrid cols={2} mt="xl" verticalSpacing={40}>
+            {postHistory.postHistories?.map((postHistory) => {
+              return (
+                <Box key={postHistory.id}>
+                  <LogSlideComponent
+                    markdown={postHistory.slide}
+                    views={formatNumber(postHistory.views)}
+                  />
+                  <Text mt={10} fw={800} align="center">
+                    {postHistory.title}
+                  </Text>
+                </Box>
+              );
+            })}
+          </SimpleGrid>
+        </Carousel.Slide>
+      </Carousel>
     </Box>
   );
 }
