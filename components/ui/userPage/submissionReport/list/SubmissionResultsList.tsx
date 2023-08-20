933ce027294c5e0d09274a329193633ccdb97eaa
Yusei Mizutani <yusei4520030401@gmail.com>
2023-08-21 04:42:15 +0900

    fix: 実績を横スクロールで閲覧できるよう変更

--- components/ui/userPage/submissionReport/list/SubmissionResultsList.tsx
+++ components/ui/userPage/submissionReport/list/SubmissionResultsList.tsx
@@ -1,55 +1,42 @@
+import { useState } from "react";
 import { useRecoilValue } from "recoil";
-import { Center, Image, Loader, SimpleGrid, Text } from "@mantine/core";
+import { Image, Loader, Text } from "@mantine/core";
 
 import { userInformationState } from "@/store/state";
-import { useUserPage } from "@/hooks/useUserPage";
-import { useEffect, useState } from "react";
+import styles from "./submissionResultsCardList.module.scss";
 
 export default function SubmissionResultsList() {
-  const { isExistsImg } = useUserPage();
   const [loading, setLoading] = useState(false);
   const userInformation = useRecoilValue(userInformationState);
-  const [imageError, setImageError] = useState(false);
 
   const handleImageError = () => {
     setLoading(false);
   };
 
-  const [isExistsImgArr, setIsExistsImgArr] = useState<boolean[]>([]);
-
   return (
-    <SimpleGrid cols={6} spacing={16} verticalSpacing={16}>
-      {userInformation.achievements.map((achievement, index) => {
+    <div className={styles.grid}>
+      {userInformation.achievements.map((achievement) => {
         return (
           <>
-            <Center key={achievement.name} h={100} w={100} variant="outline">
-              {!loading ? (
-                <Image
-                  mx="auto"
-                  radius="md"
-                  src={achievement.link}
-                  alt={achievement.name}
-                  onLoad={handleImageError}
-                  withPlaceholder
-                  placeholder={
-                    <Center
-                      key={achievement.name}
-                      bg="gray.2"
-                      h={100}
-                      w={100}
-                      variant="outline"
-                    >
-                      <Text color="black">{achievement.name}</Text>
-                    </Center>
-                  }
-                />
-              ) : (
-                <Loader />
-              )}
-            </Center>
+            {!loading ? (
+              <Image
+                key={achievement.name}
+                mx="auto"
+                radius="md"
+                height={90}
+                width={90}
+                src={achievement.link}
+                alt={achievement.name}
+                onLoad={handleImageError}
+                withPlaceholder
+                placeholder={<Text color="black">{achievement.name}</Text>}
+              />
+            ) : (
+              <Loader />
+            )}
           </>
         );
       })}
-    </SimpleGrid>
+    </div>
   );
 }
