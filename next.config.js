f89cf31eef7a341c33ed4473b5603f68ca60719f
Yusei Mizutani <yusei4520030401@gmail.com>
2023-08-22 20:47:20 +0900

    add: 開発用のサーバーと本番用のサーバーを変えれるようにコンフィグを設定

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  plugins: ["~/plugins/contentful", "~/plugins/prism"],
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+            process.env.NODE_ENV === "production"
+              ? "https://tech-tok.kajilab.tk/api/:path*" // 本番用
+              : "http://localhost:8080/api/:path*", // 開発用
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
