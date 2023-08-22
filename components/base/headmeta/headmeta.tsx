314584e85e75a9a1bc2c2b4c25361d0de92ff475
SatooRu65536 <ryuto4619@gmail.com>
2023-08-22 19:40:28 +0900

    update: Headを変更

--- components/base/headmeta/headmeta.tsx
+++ components/base/headmeta/headmeta.tsx
@@ -1,9 +1,9 @@
 import Head from "next/head";
 
-const TITLE = "アプリ名";
+const TITLE = "MoGy";
 const DESCRIPTION = "技術的なTIPSを共有するサービスです";
 const SITE_URL = "https://https://hacku-2023-nagoya.vercel.app/";
-const IMG_URL = "https://wisdom-tree.vercel.app/images/ogp.png";
+const IMG_URL = "/ogp.png";
 
 export default function HeadMeta() {
   return (
@@ -14,15 +14,9 @@ export default function HeadMeta() {
       <meta property="og:description" content={DESCRIPTION} />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={SITE_URL} />
-      <meta property="og:image" content={IMG_URL} />
-      <meta name="twitter:image" content={IMG_URL} />
+      <meta property="og:image" content={IMG_URL} />  
       <meta property="og:site_name" content={TITLE} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@SatooRu_65536" />
-      <meta name="twitter:title" content={TITLE} />
-      <meta name="twitter:description" content={DESCRIPTION} />
-      <meta name="twitter:image" content={IMG_URL} />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.png" />
     </Head>
   );
 }
