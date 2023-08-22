18df3dc5772bca601ac4efc087c4646e61de36a1
Yusei Mizutani <yusei4520030401@gmail.com>
2023-08-22 20:51:40 +0900

    fix: 数値表記から漢字を含む表記にするための関数をutilフォルダに移動

--- components/util/util.ts
+++ components/util/util.ts
@@ -1,3 +1,13 @@
 export function cloneObjec<T>(obj: T): T {
   return JSON.parse(JSON.stringify(obj));
 }
+
+export function formatNumber(views: number): string {
+  if (views >= 10000) {
+    return `${Math.floor(views / 10000)}万`;
+  } else if (views >= 1000) {
+    return `${Math.floor(views / 1000)}千`;
+  } else {
+    return views.toString();
+  }
+}
