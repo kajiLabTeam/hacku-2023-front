f8b0c9f4105f02f8f826779075ea0cbaa7b4d3fb
SatooRu65536 <ryuto4619@gmail.com>
2023-08-25 17:40:27 +0900

    change: achievementsのスタイル変更

--- components/ui/userPage/submissionReport/list/SubmissionResultsList.tsx
+++ components/ui/userPage/submissionReport/list/SubmissionResultsList.tsx
@@ -1,41 +1,36 @@
 import { useState } from "react";
 import { useRecoilValue } from "recoil";
-import { Image, Loader, Text } from "@mantine/core";
 import { userInformationState } from "@/store/state";
 import styles from "./submissionResultsCardList.module.scss";
 
 export default function SubmissionResultsList() {
-  const [loading, setLoading] = useState(false);
   const userInformation = useRecoilValue(userInformationState);
 
-  const handleImageError = () => {
-    setLoading(false);
-  };
+  const [achievements, setAchievements] = useState(
+    userInformation.achievements.filter((achievement) => {
+      return achievement.link !== "";
+    })
+  );
+
+  const [noneAchievements, setNoneAchievements] = useState(
+    userInformation.achievements.filter((achievement) => {
+      return achievement.link === "";
+    })
+  );
 
   return (
     <div className={styles.grid}>
-      {userInformation.achievements.map((achievement) => {
-        return (
-          <>
-            {!loading ? (
-              <Image
-                key={achievement.name}
-                mx="auto"
-                radius="md"
-                height={90}
-                width={90}
-                src={achievement.link}
-                alt={achievement.name}
-                onLoad={handleImageError}
-                withPlaceholder
-                placeholder={<Text color="black">{achievement.name}</Text>}
-              />
-            ) : (
-              <Loader />
-            )}
-          </>
-        );
-      })}
+      {achievements.map((achievement) => (
+        <div key={achievement.name} className={styles.have}>
+          {achievement.name}
+        </div>
+      ))}
+
+      {noneAchievements.map((achievement) => (
+        <div key={achievement.name} className={styles.none}>
+          {achievement.name}
+        </div>
+      ))}
     </div>
   );
 }
